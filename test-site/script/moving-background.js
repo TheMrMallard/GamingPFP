d4ae32d7040b7101706ee34c14bbd7d5e63e2b90
MrMallarb <jv802155@gmail.com>
2024-04-22 01:31:37 -0400

    Added more cool things

---
+++ test-site/script/moving-background.js
@@ -0,0 +1,14 @@
+var position = 0;
+
+$(document).ready(
+    () => {
+        var interval = setInterval(() => {
+            // Update position
+            position += .5;
+            // Update background position
+            $('.background').css('background-position', `${position}px ${position}px`);
+            $('#background2').css('background-position', `${position/2}px ${position/4}px`);
+
+        }, 10);
+    }
+)
